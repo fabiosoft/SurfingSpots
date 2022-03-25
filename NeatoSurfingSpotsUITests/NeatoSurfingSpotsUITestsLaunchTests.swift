b14b152f59f8e5d9d018a0b945518b922d269ed6
Fabio Nisci <fabionisci@gmail.com>
2022-03-25 22:54:30 +0100

    Project created  🚀

---
+++ NeatoSurfingSpotsUITests/NeatoSurfingSpotsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NeatoSurfingSpotsUITestsLaunchTests.swift
+//  NeatoSurfingSpotsUITests
+//
+//  Created by Fabio Nisci on 25/03/22.
+//
+
+import XCTest
+
+class NeatoSurfingSpotsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
